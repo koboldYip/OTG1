03d8e373ea95c0890ed5a5e835745f79f80bccae
koboldYip <kyldoks@gmail.com>
2022-05-05 00:15:52 +0300

    second Query+-

--- src/test/java/ru/mpei/cimmaintainer/TestClass.java
+++ src/test/java/ru/mpei/cimmaintainer/TestClass.java
@@ -1,24 +1,12 @@
 package ru.mpei.cimmaintainer;
 
-import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.query.BindingSet;
-import org.eclipse.rdf4j.query.QueryLanguage;
-import org.eclipse.rdf4j.query.TupleQuery;
-import org.eclipse.rdf4j.query.TupleQueryResult;
-import org.eclipse.rdf4j.repository.Repository;
-import org.eclipse.rdf4j.repository.RepositoryConnection;
-import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.Rio;
-import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.junit.jupiter.api.Test;
 import ru.mpei.cimmaintainer.binder.ElementsBinder;
 import ru.mpei.cimmaintainer.converter.SldToCimConverter;
 import ru.mpei.cimmaintainer.dto.SingleLineDiagram;
 import ru.mpei.cimmaintainer.mapper.JsonMapperCIM;
-
-import java.io.FileInputStream;
-import java.io.IOException;
+import ru.mpei.cimmaintainer.writer.JsonWriter;
 
 public class TestClass {
 
@@ -38,38 +26,9 @@ public class TestClass {
 
     @Test
     public void test2() {
-        String cimUri = "http://iec.ch/TC57/2013/CIM-schema-cim16#";
-        try {
-            Model results = Rio.parse(new FileInputStream("src/test/resources/asd1"),
-                    "http://iec.ch/TC57/2013/CIM-schema-cim16#",
-                    RDFFormat.RDFXML);
-            Repository repository = new SailRepository(new MemoryStore());
-            RepositoryConnection connection = repository.getConnection();
-            connection.add(results);
 
-            String queryString = "PREFIX cim: <" + cimUri + "> " +
-                    "SELECT ?tId ?cnId ?ceId " +
-                    "WHERE { " +
-                    "    ?t a cim:Terminal ; " +
-                    "       cim:IdentifiedObject.mRID ?tId ; " +
-                    "       cim:Terminal.ConductingEquipment ?ce ; " +
-                    "       cim:Terminal.ConnectivityNode ?cn . " +
-                    "    ?cn cim:IdentifiedObject.mRID ?cnId . " +
-                    "    ?cn cim:IdentifiedObject.mRID ?ceId . " +
-                    "}";
-            TupleQuery query = connection.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
-            try (TupleQueryResult result = query.evaluate()) {
-                for (BindingSet solution :
-                        result) {
-                    String tId = solution.getValue("tId").stringValue();
-                    String cnId = solution.getValue("cnId").stringValue();
-                    String ceId = solution.getValue("ceId").stringValue();
-                    System.out.println();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        JsonWriter jsonWriter = new JsonWriter();
+        jsonWriter.parseCimRdfToJson("src/test/resources/asd");
 
     }
 }
